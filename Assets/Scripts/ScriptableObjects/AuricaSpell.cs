6a5c95830c9f24137e19558d4c2389ed6756dfd5
Elliot Hume <elliot@hume.ca>
2020-12-17 15:58:57 -0500

    Start of SpellModding
    Using the Aurica system, but omitting the actual personal aura generation

---
+++ Assets/Scripts/ScriptableObjects/AuricaSpell.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AuricaSpell", menuName = "Labyrinthian/AuricaSpell", order = 1)]
+public class AuricaSpell : ScriptableObject {
+    public string c_name;
+    [TextArea(15,3)]
+    public string description;
+    public ManaDistribution targetDistribution;
+    public List<AuricaSpellComponent> keyComponents;
+    public float errorThreshold = 3.0f;
+    
+}
---
+++ Assets/Scripts/ScriptableObjects/AuricaSpellComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AuricaSpellComponent", menuName = "Labyrinthian/AuricaSpellComponent", order = 0)]
+public class AuricaSpellComponent : ScriptableObject {
+    public string c_name;
+    [TextArea(15,3)]
+    public string description;
+    public bool hasBasicDistribution, hasAuricDistribution, hasFluxDistribution;
+    public ManaDistribution basicDistribution, auricDistribution, fluxDistribution;
+    //public List<float> basicDistribution = new List<float>(7), auricDistribution = new List<float>(7), fluxDistribution = new List<float>(7);
+
+    public ManaDistribution CalculateDistributionChange(ManaDistribution givenDistribution, ManaDistribution aura){
+        ManaDistribution calculatedDistribution = new ManaDistribution(givenDistribution.ToString());
+        if (hasBasicDistribution) calculatedDistribution.AddBasicDist(basicDistribution);
+        if (hasAuricDistribution) calculatedDistribution.AddAuricDist(auricDistribution, aura);
+        if (hasFluxDistribution) calculatedDistribution.ApplyFluxDist(fluxDistribution);
+        return calculatedDistribution;
+    }
+}
